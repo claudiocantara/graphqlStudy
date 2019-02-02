4838cd92c6f3509621f159711885332debfed809
claudiocantara <claudiomar.alcantara@gmail.com>
2019-02-02 16:12:22 -0200

    Made it stateless

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql } from 'react-apollo';
 
 import Book from '../Book/';
 import {getBooksQuery} from '../../queries/'
 
 import './style.scss'
-class BookList extends Component {
-  render() {
-    const { data } = this.props;
-    return (
-      <main className={'col-small-4 col-medium-8 col-big-7 main-container'} >
-        {
-          data.loading 
-          ? <div> Loading this data... </div> 
-          : <ul className='book-list'>
-              {data.books.map(book => (
-                <Book key={book.id} name={book.name} author={book.author} genre={book.genre} id={book.id}/>
-              ))}
-          </ul>
-        }
-      </main>
-    )
-  }
-}
+const BookList = ({ data }) => (
+  <main className={'col-small-4 col-medium-8 col-big-7 main-container'} >
+    { data.loading && <div> Loading this data... </div> }
+    { data.error &&  <div> Error while Loading... </div> }
+    { data.books &&  
+      <ul className='book-list'>
+        {data.books.map(book => (
+          <Book key={book.id} name={book.name} author={book.author} genre={book.genre} id={book.id}/>
+        ))}
+      </ul>
+    }
+  </main>
+)
 
 export default graphql(getBooksQuery)(BookList);
