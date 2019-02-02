19e61367edd404364c6e79538d4751aafce24f58
claudiocantara <claudiomar.alcantara@gmail.com>
2019-02-02 16:10:36 -0200

    Modified structure and style

--- client/src/components/Author/index.js
+++ client/src/components/Author/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import './style.scss';
 
-const Author = ({name, books, age, id}) => (
-    <li>
-        <div> Name: {name} </div>
-        <div> age: {age} </div>
-    </li>  
+const Author = ({name, age }) => (
+  <li className='author-item'>
+  <p>
+    <span className='item-description'>Name:</span> 
+    {name} 
+  </p>
+  <p>
+    <span className='item-description'>Age: </span> 
+    {age} 
+  </p>
+</li>    
 )
 
-Author.defaultProps = {
-    id: '',
-    name: '',
-    books: [],
-    age: 0
-}
+
 
 export default Author;
