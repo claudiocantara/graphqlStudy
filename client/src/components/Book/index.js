6bad7267e205374f8f901b2569b0fc2a04120571
claudiocantara <claudiomar.alcantara@gmail.com>
2019-02-02 16:11:50 -0200

    Modified structure and style of Book

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -2,24 +2,17 @@ import React from 'react';
 import './style.scss'
 
 const Book = ({name, author, genre, id}) => (
-    <li className='book-item'>
-        <p>
-          <span className='item-description'>Name:</span> {name} 
-        </p>
-        <p>
-          <span className='item-description'>Author: </span> {author.name} 
-        </p>
-        <p>
-          <span className='item-description'>Gender: </span> {genre} 
-        </p>
-    </li>  
+  <li className='book-item'>
+      <p>
+        <span className='item-description'>Name:</span> {name} 
+      </p>
+      <p>
+        <span className='item-description'>Author: </span> {author.name} 
+      </p>
+      <p>
+        <span className='item-description'>Gender: </span> {genre} 
+      </p>
+  </li>  
 )
-
-Book.defaultProps = {
-    id: '',
-    name: '',
-    author: '',
-    genre: ''
-}
 
 export default Book;
